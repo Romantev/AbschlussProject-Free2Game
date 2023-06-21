43c059865686fe6547bf567f330577974f16f616
NiklasRulec <niklasrulec.98@gmail.com>
2023-06-21 14:19:03 +0200

    game detail page v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import DetailGame from "./pages/DetailGame/DetailGame";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <DetailGame />
+    </>
+  );
 }
 
 export default App;
